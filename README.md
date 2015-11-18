441013e751e1fe4e24ad32d6f08c8381443656a8
Xiaoyang Feng <xy@172-26-102-115.DYNAPOOL.NYU.EDU>
2015-11-18 11:23:39 +0800

    add python code

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# average_portrait
-Uses openCV and Dlib to automatically align portraits in python, visualizing it out using p5.js
+# Average Portrait
+Uses openCV and Dlib to automatically align portraits in python, average the portraits using p5.js
+
+## How to Use
+- Download [landmarks](http://sourceforge.net/projects/dclib/files/dlib/v18.10/shape_predictor_68_face_landmarks.dat.bz2)
+- Unzip it and move to `landmarks` folder
+- Face Align `python face_align.py image_folder image_format reference_image_path`
+- Face Detection `python face_detection.py image_folder`
+- Face Swap `python face_swap.py original_portrait.jpg new_portrait.jpg`
+
+## Examples
+Face Align
+
+```
+python face_align.py portraits/ jpg portraits/21a4462309f79052d1a480170ef3d7ca7bcbd564.jpg
+```
+
+Face Detection
+
+```
+python face_detection.py portraits/
+```
+
+Face Swap
+
+```
+python face_swap.py portraits/21a4462309f79052d1a480170ef3d7ca7bcbd564.jpg portraits/838ba61ea8d3fd1ff15d9549314e251f95ca5f68.jpg
+```
+
+## References
+- [Face Swap](http://matthewearl.github.io/2015/07/28/switching-eds-with-python/) by Matthew Earl
+- Portraits from [BaiKe](http://baike.baidu.com/view/27362.htm)
+
+## Install OpenCV and Dlib
+Install pip
+
+```
+sudo easy_install pip
+```
+
+Install cmake
+
+```
+[https://cmake.org/download/](https://cmake.org/download/)
+(Menu->Tools->How To Install For Command Line Use)
+```
+
+Install homebrew
+
+```
+ruby -e "$(curl -fsSL [https://raw.githubusercontent.com/Homebrew/install/master/install](https://raw.githubusercontent.com/Homebrew/install/master/install))"
+```
+
+Install boost
+
+```
+brew install boost --with-python
+```
+
+Install boost-python
+
+```
+brew install boost-python
+```
+
+Install numpy
+
+```
+sudo pip install numpy
+```
+
+Install dlib
+
+```
+sudo pip install dlib
+```
+
+Install opencv
+
+```
+brew tap homebrew/science
+brew install opencv (opencv installation will be located at: /usr/local/Cellar/opencv/2.4.10/)
+cat ~/.bash_profile | grep PYTHONPATH
+ln -s /usr/local/Cellar/opencv/2.4.10/lib/python2.7/site-packages/cv.py cv.py
+ln -s /usr/local/Cellar/opencv/2.4.10/lib/python2.7/site-packages/cv2.so cv2.so
+Reference:[http://www.mobileway.net/2015/02/14/install-opencv-for-python-on-mac-os-x/](http://www.mobileway.net/2015/02/14/install-opencv-for-python-on-mac-os-x/)
+```
+
+Install skimage [ OPTIONAL ]
+
+```
+sudo pip install -U scikit-image
+```
---
+++ face_align.py
@@ -0,0 +1,119 @@
+import cv2
+import dlib
+import numpy
+
+import os
+import sys
+import glob
+
+PREDICTOR_PATH = "landmarks/shape_predictor_68_face_landmarks.dat"
+FOLDER_PATH = sys.argv[1]
+IMAGE_FORMAT = sys.argv[2]
+REFERENCE_PATH = sys.argv[3]
+SCALE_FACTOR = 1
+FEATHER_AMOUNT = 11
+
+FACE_POINTS = list(range(17, 68))
+MOUTH_POINTS = list(range(48, 61))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+NOSE_POINTS = list(range(27, 35))
+JAW_POINTS = list(range(0, 17))
+
+# Points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
+                               RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+
+# Points from the second image to overlay on the first. The convex hull of each
+# element will be overlaid.
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+
+class TooManyFaces(Exception):
+    pass
+
+class NoFaces(Exception):
+    pass
+
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        raise TooManyFaces
+    if len(rects) == 0:
+        raise NoFaces
+
+    return numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+def transformation_from_points(points1, points2):
+    """
+    Return an affine transformation [s * R | T] such that:
+        sum ||s*R*p1,i + T - p2,i||^2
+    is minimized.
+    """
+    # Solve the procrustes problem by subtracting centroids, scaling by the
+    # standard deviation, and then using the SVD to calculate the rotation. See
+    # the following for more details:
+    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
+
+    points1 = points1.astype(numpy.float64)
+    points2 = points2.astype(numpy.float64)
+
+    c1 = numpy.mean(points1, axis=0)
+    c2 = numpy.mean(points2, axis=0)
+    points1 -= c1
+    points2 -= c2
+
+    s1 = numpy.std(points1)
+    s2 = numpy.std(points2)
+    points1 /= s1
+    points2 /= s2
+
+    U, S, Vt = numpy.linalg.svd(points1.T * points2)
+
+    # The R we seek is in fact the transpose of the one given by U * Vt. This
+    # is because the above formulation assumes the matrix goes on the right
+    # (with row vectors) where as our solution requires the matrix to be on the
+    # left (with column vectors).
+    R = (U * Vt).T
+
+    return numpy.vstack([numpy.hstack(((s2 / s1) * R,
+                                       c2.T - (s2 / s1) * R * c1.T)),
+                         numpy.matrix([0., 0., 1.])])
+
+def read_im_and_landmarks(fname):
+    im = cv2.imread(fname, cv2.IMREAD_COLOR)
+    im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR,
+                         im.shape[0] * SCALE_FACTOR))
+    s = get_landmarks(im)
+
+    return im, s
+
+def warp_im(im, M, dshape):
+    output_im = numpy.zeros(dshape, dtype=im.dtype)
+    cv2.warpAffine(im,
+                   M[:2],
+                   (dshape[1], dshape[0]),
+                   dst=output_im,
+                   borderMode=cv2.BORDER_TRANSPARENT,
+                   flags=cv2.WARP_INVERSE_MAP)
+    return output_im
+
+img_ref, landmark_ref = read_im_and_landmarks(REFERENCE_PATH)
+img_index = 0;
+
+for f in glob.glob(os.path.join(FOLDER_PATH, "*." + IMAGE_FORMAT)):
+    print("Processing file: {}".format(f))
+    img_index += 1
+    img, landmark = read_im_and_landmarks(f);
+    M = transformation_from_points(landmark_ref[ALIGN_POINTS],
+                                   landmark[ALIGN_POINTS])
+    warped_im2 = warp_im(img, M, img_ref.shape)
+    cv2.imwrite('result_align/' + str(img_index) + '.jpg', warped_im2)
